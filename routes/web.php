f89815d8204807cc5caacb26db72ef34b8c04f2f
Furkan Genç <27furkangenc@gmail.com>
2026-09-22 16:35:33 +0300

    Throttle değiştirildi

--- routes/web.php
+++ routes/web.php
@@ -101,7 +101,7 @@ Route::get('queuework', function () {
 | olarak TASLAK düşer, yayın insan kararıdır.
 */
 Route::get('/otomatik-blog/{secret}', [AutoBlogController::class, 'generate'])
-    ->middleware('throttle:20,60')
+    ->middleware('throttle:25,5')
     ->name('otomatik-blog.generate');
 
 Route::get('/media/{media}/player', [PlayerController::class, 'show'])
