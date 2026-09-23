186871d0cee64d37371c16f14f8a6d0def611e17
Furkan Genç <27furkangenc@gmail.com>
2026-09-23 22:48:54 +0300

    Adjust throttle middleware for automatic blog route from 4 to 2 requests per minute to improve request management.

--- routes/web.php
+++ routes/web.php
@@ -101,7 +101,7 @@ Route::get('queuework', function () {
 | olarak TASLAK düşer, yayın insan kararıdır.
 */
 Route::get('/otomatik-blog/{secret}', [AutoBlogController::class, 'generate'])
-    ->middleware('throttle:4,60')
+    ->middleware('throttle:2,60')
     ->name('otomatik-blog.generate');
 
 Route::get('/media/{media}/player', [PlayerController::class, 'show'])
