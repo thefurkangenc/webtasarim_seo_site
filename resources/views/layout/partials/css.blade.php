3ae7a41ebd904ed4f87888ed7352bad89487f660
Furkan Genç <27furkangenc@gmail.com>
2026-09-24 14:23:59 +0300

    css version

--- resources/views/layout/partials/css.blade.php
+++ resources/views/layout/partials/css.blade.php
@@ -13,6 +13,7 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 <!--=====CSS=======-->
+
 <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/fontawesome.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/magnific-popup.css') }}">
@@ -23,7 +24,7 @@
 <link rel="stylesheet" href="{{ asset('assets/css/mobile-menu.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/utility.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/main.css?v=21') }}">
-<link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
+<link rel="stylesheet" href="{{ asset('assets/css/custom.css?v=1.0.1') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/layout/header.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/integrations.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/references.css') }}">
